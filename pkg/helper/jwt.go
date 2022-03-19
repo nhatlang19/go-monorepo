be463ce1d2963d0901b297e507e3b7a80b549be2
Noctis <noctis@doahub.com>
2022-03-19 22:20:32 +0700

    jwt handle

--- pkg/helper/jwt.go
+++ pkg/helper/jwt.go
@@ -1,13 +1,28 @@
 package helper
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
 
-func CreateToken(userid uint64) (string, error) {
+type JwtToken interface {
+	CreateToken(userid uint64) (string, error)
+	ExtractToken(bearToken string) string
+	VerifyToken(token string) (*jwt.Token, error)
+}
+
+type jwtToken struct {
+}
+
+func NewJwtToken() JwtToken {
+	return jwtToken{}
+}
+
+func (j jwtToken) CreateToken(userid uint64) (string, error) {
 	var err error
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
@@ -18,5 +33,29 @@ func CreateToken(userid uint64) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	return token, nil
+}
+
+func (j jwtToken) ExtractToken(bearToken string) string {
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+func (j jwtToken) VerifyToken(token string) (*jwt.Token, error) {
+	tokenString := j.ExtractToken(token)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	return token, nil
 }
