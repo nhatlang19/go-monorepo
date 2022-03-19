dca53c3f5e7c6145fd7f90c6fa3700a7a8746826
Noctis <noctis@doahub.com>
2022-03-19 17:23:17 +0700

    some changes

--- services/user/main.go
+++ services/user/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/nhatlang19/go-monorepo/services/user/model"
 	"github.com/nhatlang19/go-monorepo/services/user/route"
+	model "github.com/nhatlang19/go-monorepo/pkg/db"
 )
 
 func main() {
